0511458ef0768b73bf52e68af5763d10bacfcc2b
lrq-git <13509798192@163.com>
2026-02-22 22:48:57 +0800

    2026.2.21

---
+++ JavaScript/01js的基本语法/JS/文档注释.js
@@ -0,0 +1,8 @@
+/**
+ * 和大家打招呼
+ * @param {string} name - 姓名
+ * @param {number} age - 年龄
+ */
+function sayhello(name, age) {
+  console.log(name + " say hello! you are " + age + " years old.");
+}
